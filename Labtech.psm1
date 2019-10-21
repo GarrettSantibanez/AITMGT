bae3f3aa541e3644b8758977c0406492b9f178c0
GarrettSantibanez <56527975+GarrettSantibanez@users.noreply.github.com>
2019-10-21 15:47:33 -0700

    Update Labtech.psm1

--- Labtech.psm1
+++ Labtech.psm1
@@ -678,7 +678,7 @@ Function Uninstall-LTService{
             If (-not ($GoodServer)) {
                 If ($Svr -match '^(https?://)?(([12]?[0-9]{1,2}\.){3}[12]?[0-9]{1,2}|[a-z0-9][a-z0-9_-]+(\.[a-z0-9][a-z0-9_-]*)*)$') {
                     Try{
-                        If ($Svr -notmatch 'https?://.+') {$Svr = "http://$($Svr)"}
+                        If ($Svr -notmatch 'https?://.+') {$Svr = "https://$($Svr)"}
                         $SvrVerCheck = "$($Svr)/Labtech/Agent.aspx"
                         Write-Debug "Line $(LINENUM): Testing Server Response and Version: $SvrVerCheck"
                         $SvrVer = $Script:LTServiceNetWebClient.DownloadString($SvrVerCheck)
